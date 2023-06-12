d8aaf530ee20aa4a82c30921e3efa843f41e0b29
Trarkinhos <marcoscarvalho.dev@gmail.com>
2023-06-12 09:46:10 -0300

    Concluida

--- 018 - seno, cosseno e tangente.py
+++ 018 - seno, cosseno e tangente.py
@@ -1,4 +1,4 @@
-# Faça um programa que ielai um ângulo qualquer e mostre na tela o valor do
+# Faça um programa que leia um ângulo qualquer e mostre na tela o valor do
 # seno, cosseno e tangente desse ângulo.
 # Exemplo: Seno: 0.71 | Cosseno: 0.71 | Tangente: 1.00
 
